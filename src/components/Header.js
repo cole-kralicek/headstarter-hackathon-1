f8cab8f22386c914d3db288d1814781d355d4018
Allison Cui <cuijy@umich.edu>
2024-07-28 03:13:48 -0400

    updated library page (and card), started styling popup)

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ import './stylesheets/Header.css';
 
 function Header() {
     return (
-        <header>
-            {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <header className="head">
+            {/* <link rel="preconnect" href="https://fonts.googleapis.com/" />
+            <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" /> */}
             <div className="logo">
                 <a href='/'>Backseat</a>
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,62 +1,64 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Card from '../components/Card.js'
 import Popup from '../components/Popup.js'
-import '../App.css'
+import Header from '../components/Header.js';
+import Footer from '../components/Footer.jsx';
 
-const Library = ()=> {
-const [selectedGame, setSelectedGame] = useState(null);
-const [isPopupVisible, setPopupVisible] = useState(false);
+const Library = () => {
+  const [selectedGame, setSelectedGame] = useState(null);
+  const [isPopupVisible, setPopupVisible] = useState(false);
 
-const games = [
+  const games = [
     { id: 1, name: 'Elden Ring', year: '2024', activePlayers: '5000', description: 'Game description here', image: '../imgs/elden-ring.webp' },
     { id: 2, name: 'COD', year: '2024', activePlayers: '5000', description: 'Game description here', image: '../imgs/elden-ring.webp' }
-];
+  ];
 
 
   const handleCardClick = (game) => {
     setPopupVisible(true);
-    setSelectedGame(game);  
+    setSelectedGame(game);
   };
 
   const handleClosePopup = () => {
     setPopupVisible(false);
-    setSelectedGame(null);  
+    setSelectedGame(null);
   };
 
 
   return (
     <div>
-      <main>
-      <h1>My Library</h1>
-        <h2>Owned</h2>
+      <Header />
+      <main className='library-container'>
+        <h1 className='library-title'>My Library</h1>
+        <h2 className='game-status'>Owned</h2>
         <div className="gameList">
           <ul>
-          {games.map(game => (
+            {games.map(game => (
               <li key={game.id}>
-                <Card game={game} onClick={() => handleCardClick(game)}/>
+                <Card game={game} onClick={() => handleCardClick(game)} />
               </li>
             ))}
-        </ul>
+          </ul>
         </div>
-        <h2>Wishlist</h2>
+        <h2 className='game-status'>Wishlist</h2>
         <div className='gameList'>
-        <ul>
-          {games.map(game => (
+          <ul>
+            {games.map(game => (
               <li key={game.id}>
-                <Card game={game} onClick={() => handleCardClick(game)}/>
+                <Card game={game} onClick={() => handleCardClick(game)} />
               </li>
             ))}
             {isPopupVisible && (
-        <Popup
-          game={selectedGame}
-          onClose={handleClosePopup}
-        />
-      )}
-        </ul>
+              <Popup
+                game={selectedGame}
+                onClose={handleClosePopup}
+              />
+            )}
+          </ul>
         </div>
       </main>
-
-
+      <Footer />
+      {/* <div className={`overlay ${isPopupVisible ? 'visible' : ''}`}></div> */}
     </div>
   );
 }
